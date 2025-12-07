98a3c0b20bda4007e59692baf6a7d5436e683e78
Lloyd Everett <lloydeverett@gmail.com>
2025-12-07 09:07:44 +0200

    highlight fixes, put number after nvim entries

--- lua/treectl/highlights.lua
+++ lua/treectl/highlights.lua
@@ -5,7 +5,7 @@ M.Comment = "treectl_Comment"
 M.Hidden = "treectl_Hidden"
 M.Directory = "treectl_Directory"
 M.ErrorMsg = "treectl_ErrorMsg"
-M.Number = "treectl_Number"
+M.Special = "treectl_Special"
 M.Debug = "treectl_Debug"
 M.IndicatorActive = "treectl_IndicatorActive"
 M.IndicatorInactive = "treectl_IndicatorInactive"
@@ -15,17 +15,21 @@ M.TreeModBuiltins = "treectl_TreeModBuiltins"
 M.TreeModOther = "treectl_TreeModOther"
 
 function M.configure()
-    vim.api.nvim_set_hl(0, M.TreeModFs,         { fg = vim.g["terminal_color_" .. 2], ctermfg = 2, force = true })
-    vim.api.nvim_set_hl(0, M.TreeModNvim,       { fg = vim.g["terminal_color_" .. 5], ctermfg = 5, force = true })
-    vim.api.nvim_set_hl(0, M.TreeModBuiltins,   { fg = vim.g["terminal_color_" .. 6], ctermfg = 6, force = true })
-    vim.api.nvim_set_hl(0, M.TreeModOther,      { fg = vim.g["terminal_color_" .. 4], ctermfg = 4, force = true })
+    local function define_hl(name, color_index)
+        vim.api.nvim_set_hl(0, name, { fg = vim.g["terminal_color_" .. color_index], ctermfg = color_index, force = true })
+    end
+    define_hl(M.Special,         3)
+    define_hl(M.TreeModFs,       2)
+    define_hl(M.TreeModNvim,     5)
+    define_hl(M.TreeModBuiltins, 6)
+    define_hl(M.TreeModOther,    4)
+
     vim.api.nvim_set_hl(0, M.Comment,           { link = "Comment"     })
     vim.api.nvim_set_hl(0, M.Hidden,            { link = "Comment"     })
     vim.api.nvim_set_hl(0, M.Directory,         { link = M.TreeModFs   })
     vim.api.nvim_set_hl(0, M.ErrorMsg,          { link = "ErrorMsg"    })
-    vim.api.nvim_set_hl(0, M.Number,            { link = "Number"      })
-    vim.api.nvim_set_hl(0, M.Debug,             { link = "SpecialChar" })
-    vim.api.nvim_set_hl(0, M.IndicatorActive,   { link = "SpecialChar" })
+    vim.api.nvim_set_hl(0, M.Debug,             { link = M.Special     })
+    vim.api.nvim_set_hl(0, M.IndicatorActive,   { link = M.Special     })
     vim.api.nvim_set_hl(0, M.IndicatorInactive, { link = "LineNr"      })
 end
 
